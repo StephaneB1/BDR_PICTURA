99149446ab8d4ac9016267f7568d70601cd20aa7
Simon <simon.mattei@heig-vd.ch>
2020-01-09 21:31:22 +0100

    TRIGGERS, INSERTS and MR updated

--- SQL/Insert_tests.sql
+++ SQL/Insert_tests.sql
@@ -4,11 +4,64 @@
 --   		              Robin Demarta &
 --                        Simon Mattei
 -- Date du rendu :        13.12.2019
--- -----------------------------------------------------   
+-- ----------------------------------------------------- 
+
+USE PICTURA; 
 
 -- Ajout d'utilisateurs
+
+
 INSERT INTO utilisateur VALUES ("Steph", "stephane@heig-vd.ch", "76t824z9uoigtjknf", "Bottin", "Stephane");
 INSERT INTO utilisateur VALUES ("Robin", "robin@heig-vd.ch", "4tb9eu0r2jio4", "Demarta", "Robin");
 INSERT INTO utilisateur VALUES ("Simon", "simon@heig-vd.ch", "dfkjhfor3t5t54", "Mattei", "Simon");
 
-SELECT * FROM utilisateur;
+INSERT INTO Communaute VALUES ("Chats","cest mignon",NULL);
+INSERT INTO Communaute VALUES ("Chiens","cest mignon",NULL);
+
+INSERT INTO Balise VALUES ("funny");
+INSERT INTO Balise VALUES ("cool");
+INSERT INTO Balise VALUES ("sick");
+
+INSERT INTO Photo VALUES ("1","chat",NULL,"2009-10-10 15:30:20","0","dot.com","Steph","Chats");
+INSERT INTO Photo VALUES ("2","chat2",NULL,"2001-10-10 15:30:20","0","dot.com","Simon","Chats");
+INSERT INTO Photo VALUES ("3","chien",NULL,"2001-10-10 15:30:20","0","dot.com","Robin","Chiens");
+
+INSERT INTO Commentaire VALUES ("2010-10-10 15:30:30","1","Simon","undeuxtrois");
+INSERT INTO Commentaire VALUES ("2010-10-10 15:30:30","2","Simon","undeuxtrois");
+
+
+INSERT INTO Commentaire VALUES ("2010-10-10 14:30:30","2","Steph","c'est nul");
+INSERT INTO Commentaire VALUES ("2011-10-10 14:30:30","1","Steph","c'est nul");
+
+-- INSERT INTO Reponse_Commentaire VALUES ("2010-10-10 14:30:30","2","2010-10-10 15:30:30","2"); TRIGGER reponse_bonne_date
+INSERT INTO Reponse_Commentaire VALUES ("2011-10-10 14:30:30","1","2010-10-10 15:30:30","1");
+-- INSERT INTO Reponse_Commentaire VALUES ("2011-10-10 14:30:30","1","2010-10-10 15:30:30","2"); TRIGGER reponse_meme_photo
+
+-- INSERT INTO Commentaire VALUES ("2009-09-10 14:30:30","1","Robin","j'adore tester la BD"); TRIGGER commentaire_bonne_date
+
+INSERT INTO Utilisateur_Modere_Communaute VALUES ("Steph","Chats","1");
+INSERT INTO Utilisateur_Modere_Communaute VALUES ("Simon","Chats","1");
+INSERT INTO Utilisateur_Modere_Communaute VALUES ("Robin","Chats","0");
+INSERT INTO Utilisateur_Modere_Communaute VALUES ("Steph","Chiens","1");
+INSERT INTO Utilisateur_Modere_Communaute VALUES ("Simon","Chiens","1");
+INSERT INTO Utilisateur_Modere_Communaute VALUES ("Robin","Chiens","0");
+
+UPDATE Utilisateur_Modere_Communaute
+SET niveauPrivilege = 0
+WHERE pseudoUtilisateur = "Simon" AND nomCommunaute = "Chats";
+
+/*
+UPDATE Utilisateur_Modere_Communaute
+SET niveauPrivilege = 0
+WHERE pseudoUtilisateur = "Steph" AND nomCommunaute = "Chats";
+*/
+-- TRIGGER admin_update
+
+/*
+DELETE FROM Utilisateur_Modere_Communaute
+WHERE pseudoUtilisateur = "Steph";
+ -- TRIGGER admin_delete
+*/
+
+INSERT INTO Photo_Balise VALUES("1","funny");
+INSERT INTO Photo_Balise VALUES("2","cool");
