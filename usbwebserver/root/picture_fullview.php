83dced9a02cb306948cf007ef927f43a9a6e1fad
Robin <sikiewz@gmail.com>
2020-01-19 23:28:33 +0100

    Fullview's exit button: go back to previous page instead of index

--- usbwebserver/root/picture_fullview.php
+++ usbwebserver/root/picture_fullview.php
@@ -55,7 +55,7 @@ $community = $db->getCommunityByName($picture["nomCommunaute"])[0];
                     <div class="pictureHeaderRight">
                         <div class="pictureFullHeaderTitle">' . htmlentities($picture["titre"]) . '</div>
                         <div class="pictureFullHeaderSubtitle">' . htmlentities($picture["pseudoUtilisateur"]) . ' • ' . formatDate($picture["dateHeureAjout"], "d.m.Y, H:i") . '</div>
-                        <a id="exitpopup" href="index.php">X</a>
+                        <a id="exitpopup" href="javascript:;" onclick="window.history.go(-1); return false;">X</a>
                     </div>
 
 
